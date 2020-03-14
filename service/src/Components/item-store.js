999839e781cca49ca354fa4dc1b9aa796cdc5bf3
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2020-03-14 12:04:41 -0600

    fix security problem

--- service/src/Components/item-store.js
+++ service/src/Components/item-store.js
@@ -2,34 +2,52 @@ import { LitElement, html } from 'lit-element';
 import { connect } from 'pwa-helpers/connect-mixin.js';
 import { store } from '../Redux/store.js';
 
+function privateItem() {
+  let item = {};
+  function setItem(value) {
+    item = value;
+  }
+  function getItem() {
+    return item;
+  }
+  return {
+    getItem,
+    setItem
+  }
+}
+
+let item = privateItem();
 class ItemStore extends connect(store)(LitElement) {
   static get properties() {
     return {
-      price: { type: Number },
-      quantity: { type: Number },
-      image: { type: String },
-      name: { type: String },
-      code: { type: String }
+      price: { type: Number, hasChanged() { return false } },
+      quantity: { type: Number, hasChanged() { return false } },
+      image: { type: String, hasChanged() { return false } },
+      name: { type: String, hasChanged() { return false } },
+      code: { type: String, hasChanged() { return false } }
     }
   }
-  checkOut() {
+  firstUpdated() {
+    item.setItem({
+      price: this.price,
+      quantity: this.quantity,
+      image: this.image,
+      name: this.name,
+      code: this.code
+    });
+  }
+  _checkOut() {
     store.dispatch({ type: 'CLEAR_CART' })
     store.dispatch({
       type: 'ADD_ITEM',
-      item: {
-        price: this.price,
-        quantity: this.quantity,
-        image: this.image,
-        name: this.name,
-        code: this.code
-      }
+      item: item.getItem()
     })
     store.dispatch({ type: 'OPEN_CART' })
 
   }
   render() {
     return html`
-        <slot @click="${this.checkOut}"></slot>
+        <slot @click="${this._checkOut}"></slot>
     `;
   }
 }
